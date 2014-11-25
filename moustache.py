3c5fdf86a1bebd012a4d076762c55e26ffdee958
Chris Renard <Christopher.Renard@gmail.com>
2014-11-25 00:36:43 -0600

    fixed rotation issues (note: requires overlay images to be square to function properly) moved scale outside draw (if you're going to use rotation, you need to scale before rotating)

--- moustache.py
+++ moustache.py
@@ -2,13 +2,12 @@ import cv2
 import math
 import numpy
 
-def draw(src, overlay, face_y, face_x, face_w):
-	overlay = scale_to(overlay,face_w)
+def draw(src, overlay, face_y, face_x):
 	height, width = overlay.shape[:2]
 	dst = src.copy()
 	d_height, d_width = dst.shape[:2]
 	
-	print "[",width,",",height,"]"
+	#print "[",width,",",height,"]"
 	
 	for y in range (0, height):
 		for x in range (0, width):
@@ -17,10 +16,10 @@ def draw(src, overlay, face_y, face_x, face_w):
 			
 			if (dest_x >= 0) and (dest_x < d_width) and (dest_y >= 0) and (dest_y < d_height):
 				if overlay[y][x].any() != 0:
-					print "(",x,",",y,") -> (",dest_x,",",dest_y,")"
+					#print "(",x,",",y,") -> (",dest_x,",",dest_y,")"
 					dst[dest_y][dest_x] = blend(dst[dest_y][dest_x], overlay[y][x])
 
-	cv2.imwrite("debug.png", overlay)
+	#cv2.imwrite("debug.png", overlay)
 	return dst
 
 def translate(cord,scale,position):
@@ -33,7 +32,7 @@ def blend(base, overlay):
 def compute_scale(overlay, target):
 	height, width = overlay.shape[:2]
 	scale = 1.0 * target / width
-	print "scale: (",width,",",height,")[",target,"] --> ",scale
+	#print "scale: (",width,",",height,")[",target,"] --> ",scale
 	return (int(width * scale),int(height * scale))
 
 def scale_to(overlay, target):
@@ -43,52 +42,57 @@ def process_frame(frame,dst_fname):
 	face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 	mouth_cascade = cv2.CascadeClassifier('haarcascade_mcs_mouth.xml')
 	eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+	#snose_cascade = cv2.CascadeClassifier('haarcascade_mcs_nose.xml')
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	
 	mustache = cv2.imread('CurlyMustache.png',-1)
 	monocle = cv2.imread('monocle.png',-1)
 	visor = cv2.imread('visor_b.png',-1)
+	target = cv2.imread('target.png',-1)
 	
 	faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2,
                         minNeighbors=4,
                         flags=cv2.cv.CV_HAAR_SCALE_IMAGE |
                         cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT)
 	for (x, y, w, h) in faces:
-		print "(",x,",",y,") [",w,",",h,"]"
+		#print "(",x,",",y,") [",w,",",h,"]"
 		face_gray = gray[y:y+h, x:x+w]
-		'''mouth = mouth_cascade.detectMultiScale(face_gray,
+		mouth = mouth_cascade.detectMultiScale(face_gray,
 						flags=cv2.cv.CV_HAAR_SCALE_IMAGE |
                         cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT)
 		(mx, my, mw, mh) = mouth[0]
-		print "(",mx,",",my,") [",mw,",",mh,"]"
-		print "<",(my + y + mh / 2),",",(mx + x + mw / 2),">"
-		frame = draw(frame, mustache, my + y + mh / 2, mx + x + mw / 2, w)
-		eye = eye_cascade.detectMultiScale(face_gray,
+		#print "(",mx,",",my,") [",mw,",",mh,"]"
+		#print "<",(my + y + mh / 2),",",(mx + x + mw / 2),">"
+		mustache = scale_to(mustache, w)
+		frame = draw(frame, mustache, my + y + mh / 2, mx + x + mw / 2)
+		'''eye = eye_cascade.detectMultiScale(face_gray,
 						flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
 		(ex, ey, ew, eh) = eye[1]
 		print "(",ex,",",ey,") [",ew,",",eh,"]"
 		print "<",(ey + y + eh / 2),",",(ex + x + ew / 2),">"
-		frame = draw(frame, monocle, ey + y + eh / 2, ex + x + ew / 2, int(w/3.5))'''
+		monocle = scale_to(monocle, int(w/3.5))
+		frame = draw(frame, monocle, ey + y + eh / 2, ex + x + ew / 2)'''
 		eye = eye_cascade.detectMultiScale(face_gray,
 						flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
 		x1,y1 = box_center(*eye[0])
 		x2,y2 = box_center(*eye[1])
-		print "(",x1,",",y1,") (",x2,",",y2,")"
-		print "<",((y1 + y2) / 2 + y),",",((x1 + x2) / 2 + x),">"
-		#visor = align_slope(visor,x1,y1,x2,y2)
-		frame = draw(frame,visor, (y1 + y2) / 2 + y, (x1 + x2) / 2 + x, w)
+		#print "(",x1,",",y1,") (",x2,",",y2,")"
+		#print "<",((y1 + y2) / 2 + y),",",((x1 + x2) / 2 + x),">"
+		visor = scale_to(visor,w)
+		visor = align_slope(visor,x1,y1,x2,y2)
+		frame = draw(frame,visor, (y1 + y2) / 2 + y, (x1 + x2) / 2 + x)
 
 	cv2.imwrite(dst_fname, frame)
 
 def align_slope(overlay,x1,y1,x2,y2):
 	height, width = overlay.shape[:2]
-	print "[",width,",",height,"]"
+	#print "[",width,",",height,"]"
 	theta = math.degrees(math.atan2((y1 - y2),(x1 - x2)))
-	print "theta: ",theta
-	matrix = cv2.getRotationMatrix2D((height/2,width/2),90,1)
-	img = cv2.warpAffine(overlay,matrix,(height, width))
+	#print "theta: ",theta
+	matrix = cv2.getRotationMatrix2D((height/2,width/2),-theta,1)
+	img = cv2.warpAffine(overlay,matrix,(width, width))
 	height, width = img.shape[:2]
-	print "[",width,",",height,"]"
+	#print "[",width,",",height,"]"
 	return img
 
 def box_center(x,y,w,h):
